7b3a9d51ca24a283c749fb1d938d4db9bb07b886
d0422 <rlfehd2013@naver.com>
2022-03-21 11:33:35 +0900

    1972

---
+++ 1972.py
@@ -0,0 +1,17 @@
+while (1):
+    l=input()
+    if l=='*':
+        break
+    check=1
+    for x  in range(1,len(l)):
+        result=[]
+        for y in range(len(l)):
+            if y+x<len(l):
+                result.append(l[y]+l[x+y])
+        for t in range(len(result)):
+            if result[t] in result[t+1:]:
+                check=0
+    if check:
+        print(l+" is surprising.")
+    else:
+        print(l+" is NOT surprising.")
