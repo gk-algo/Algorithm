ccc2822ee8cd14de85d02f6899e7e9f40169f27f
d0422 <rlfehd2013@naver.com>
2022-04-01 13:08:27 +0900

    2075

---
+++ 2075.py
@@ -0,0 +1,16 @@
+#https://www.acmicpc.net/problem/2075
+import heapq
+n=int(input())
+heap=[]
+for _ in range(n):
+    l=list(map(int,input().split()))
+    for x in l:
+        if len(heap)<n:
+            heapq.heappush(heap,x)
+        else:
+            t=heapq.heappop(heap)
+            if t<x:
+                heapq.heappush(heap,x)
+            else:
+                heapq.heappush(heap,t)
+print(heapq.heappop(heap))
