6ebd00d4fc5372b9a1459c3eb80d5c5656ea0e5c
d0422 <rlfehd2013@naver.com>
2022-07-19 22:00:22 +0900

    :beers: 맥주파티

--- 백준/자료구조/17503.py
+++ 백준/자료구조/17503.py
@@ -1,23 +1,31 @@
-def bin(x, start, end):
-    mid=(start+end)//2
-    if l[mid][0]>x:
-        bin(x,start,mid)
-    elif l[mid][0]<x:
-        bin(x,mid,end)
-    else:
-        return mid
-
+import sys
 import heapq
+input=sys.stdin.readline
 n,m,k=map(int,input().split())
-l=[]
-maxnum=0
+heap=[]
+answer=[]
 for _ in range(k):
-    a,b=map(int,input().split())
-    if b>maxsize:
-        maxsize=b
-    l.append([b,a])
-l.sort()
-for x in range(maxsize):
-    start=bin(x,0,len(l))
-    while l[start][0]!=x:
-        start+=1
+    prefer,alchol=list(map(int,input().split()))
+    heapq.heappush(heap,[alchol,prefer])
+s=0
+maxalchol=0
+second_alchol=0
+# heap에는 알콜이 낮은 순으로 들어감
+# answer에는 perfer가 낮은 순으로 들어감
+while heap:
+    alchol, prefer=heapq.heappop(heap)
+    if maxalchol<alchol:
+        secondalchol=maxalchol
+        maxalchol=alchol
+    s+=prefer
+    heapq.heappush(answer,[prefer,alchol])
+    if len(answer)==n:
+        if s>=m:
+            print(maxalchol)
+            exit()
+        else:
+            x=heapq.heappop(answer)
+            s-=x[0]
+            if x[1]==maxalchol:
+                maxalchol=second_alchol
+print(-1)
