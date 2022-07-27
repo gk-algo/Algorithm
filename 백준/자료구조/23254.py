8e5d91ce1dcf5ef2f223ba2bfad688194df5b77c
d0422 <rlfehd2013@naver.com>
2022-07-28 00:38:52 +0900

    :zap: 23254
    드디어품

--- 백준/자료구조/23254.py
+++ 백준/자료구조/23254.py
@@ -5,7 +5,6 @@ time=24*n
 # m과목
 a=list(map(int,input().split()))
 b=list(map(int,input().split()))
-단위별점수=dict()
 결과=0
 힙=[]
 for x in range(m):
@@ -13,27 +12,19 @@ for x in range(m):
 while 힙:
     힙에서뽑은거=heapq.heappop(힙)
     단위,점수=-힙에서뽑은거[0],힙에서뽑은거[1]
-    while 단위+점수<100:
+    while 단위+점수<=100:
         if time>0:
             점수+=단위
             time-=1
         else:
             break
-    if 단위별점수.get(단위):
-        heapq.heappush(단위별점수[단위],점수)
+    if 점수==100:
+        결과+=100
     else:
-        단위별점수[단위]=[]
-        heapq.heappush(단위별점수[단위],점수)
-    if 힙:
-        while 단위별점수[단위]:
-            if (100-단위별점수[단위][0])>=-힙[0][0] and time>0:
-                heapq.heappop(단위별점수[단위])
-                결과+=100
-                time-=1
-            else:
-                break
-print(단위별점수)
-for 키 in 단위별점수.keys():
-    for 점수 in 단위별점수[키]:
-        결과+=점수
+        heapq.heappush(힙,[-(100-점수),점수])
+    if time==0:
+        break
+while 힙:
+    필요없는거, 점수=heapq.heappop(힙)
+    결과+=점수
 print(결과)
